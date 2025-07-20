c8a6c056375444c18a7604a47c240e6e25e7cad5
nextdev42 <jonathan.mrutu@protonmail.com>
2025-07-20 19:02:10 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
-# app/__init__.py
-
 from flask import Flask
-#from flask_bootstrap import Bootstrap
 
 def create_app():
     app = Flask(__name__)
-    Bootstrap(app)
 
     from app.routes import main
     app.register_blueprint(main)
